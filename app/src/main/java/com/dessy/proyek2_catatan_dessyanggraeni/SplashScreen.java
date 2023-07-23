30dd4ca610633e3f1740422bac5c0363f9c9b2eb
Dessy Anggraeni <alienaasibelajar@gmail.com>
2023-07-23 18:01:55 +0700

    Catatan

---
+++ app/src/main/java/com/dessy/proyek2_catatan_dessyanggraeni/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.dessy.proyek2_catatan_dessyanggraeni;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int splash_time_out=3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },splash_time_out);
+    }
+}
